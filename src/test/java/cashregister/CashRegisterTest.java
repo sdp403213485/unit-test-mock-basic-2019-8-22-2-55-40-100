ae7d750c39324b52898711a2e9613a968512ec02
sdp <403213485@qq.com>
2019-08-26 16:48:07 +0800

    1

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -3,6 +3,8 @@ package cashregister;
 
 import org.junit.jupiter.api.Test;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
@@ -10,9 +12,31 @@ public class CashRegisterTest {
 
     @Test
     public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+//        //given
+//    	Item [] itemArr= new Item[1];
+//    	itemArr[0] = new Item("apple", 2);
+//    	itemArr[1] = new Item("banana", 6);
+//    	
+//    	Purchase purchase = new Purchase(itemArr);
+//    	Printer printer = null;
+//		CashRegister cashRegister = new CashRegister(printer);
+//    	
+//        //when
+//		cashRegister.process(purchase);
+//        //then
+//		assertEquals(,cashRegister.process(purchase));
+    	
+    	Printer printer = mock(Printer.class);
+    	Purchase purchase = mock(Purchase.class);
+    	CashRegister cashRegister =  new CashRegister(printer);
+    	when(purchase.asString()).thenReturn("the message which should be print");
+    	
+    	//when
+    	cashRegister.process(purchase);
+    	 
+    	//then
+    	verify(printer).print("the message which should be print");
+    	
     }
 
     @Test
@@ -21,6 +45,10 @@ public class CashRegisterTest {
         //when
         //then
     }
+    
+    
+    
+    
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
